534c2cfa76d64c021c7b85506ec9ccbe02549127
Schwi16 <73543165+Schwi16@users.noreply.github.com>
2022-03-16 21:02:58 +0700

    first commit

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    private SpriteRenderer sr;
+
+    public Color color1;
+    public Color color2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        StartCoroutine(ChangeColor());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator ChangeColor()
+    {
+        while(true)
+        {
+            if (sr.color == color1)
+            {
+                sr.color = color2;
+            }
+            else sr.color = color1;
+            yield return new WaitForSeconds(3);
+        }
+    }
+}
